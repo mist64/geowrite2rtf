78b9c081cce7c6c6f8ee07dc54f579cb5de207c9
Michael Steil <mist64@mac.com>
2015-01-27 03:20:08 +0100

    RTF output support

--- geowrite2txt.c
+++ geowrite2txt.c
@@ -31,10 +31,10 @@ int
 main(int argc, char **argv)
 {
 	if (argc < 3) {
-		fprintf(stderr, "Usage: %s <infile> <outfile>\n", basename(argv[0]));
-		fprintf(stderr, "\nThis tool converts a C64/C128 GEOS GeoWrite .CVT file into an HTML or\n");
-		fprintf(stderr, "plain-text file, depending on whether <outfile> ends in '.html' or not.\n");
-		fprintf(stderr, "All graphics information will be discarded.\n");
+		fprintf(stderr, "Usage: %s <infile> <outfile.rtf|outfile.html|outfile.txt>\n", basename(argv[0]));
+		fprintf(stderr, "\nThis tool converts a C64/C128 GEOS GeoWrite .CVT file into an RTF, HTML,\n");
+		fprintf(stderr, "or plain-text file, depending on the file extension of <outfile>.\n");
+		fprintf(stderr, "All graphics information will be discarded. Choose RTF for best results.\n");
 		return 1;
 	}
 
@@ -43,11 +43,17 @@ main(int argc, char **argv)
 
 	// find out whether we're supposed to output HTML
 	int print_html = 0;
+	int print_rtf = 0;
 	size_t outfile_len = strlen(outfile);
-	const char *suffix = ".html";
-	size_t suffix_len = strlen(suffix);
-	if (outfile_len >= suffix_len && !strcmp(outfile + outfile_len - suffix_len, suffix) ){
+	const char *html_suffix = ".html";
+	const char *rtf_suffix = ".rtf";
+	size_t html_suffix_len = strlen(html_suffix);
+	size_t rtf_suffix_len = strlen(rtf_suffix);
+	if (outfile_len >= html_suffix_len && !strcmp(outfile + outfile_len - html_suffix_len, html_suffix) ){
 		print_html = 1;
+	}
+	if (outfile_len >= rtf_suffix_len && !strcmp(outfile + outfile_len - rtf_suffix_len, rtf_suffix) ){
+		print_rtf = 1;
 	}
 
 	// read the whole file into memory
@@ -73,6 +79,10 @@ main(int argc, char **argv)
 	// this is where the payload starts in the file
 	char *payload = &data[0x2FA];
 
+	if (print_rtf) {
+		fprintf(f, "{\\rtf1 ");
+	}
+
 	// the record data is 127 two byte records starting here
 	unsigned char *record = (unsigned char *)&data[0x1FC];
 	for (int i = 0; i < 61; i++) { // max 61 pages
@@ -97,6 +107,7 @@ main(int argc, char **argv)
 		}
 
 		char style = 0;
+		int font_size = 0;
 		for (int j = 0; j < chain_size; j++) {
 			unsigned char c = payload[j];
 
@@ -127,6 +138,8 @@ main(int argc, char **argv)
 				case 0x0d:
 					if (print_html) {
 						fprintf(f, "<br/>");
+					} else if (print_rtf) {
+						fprintf(f, "\\\n");
 					} else {
 						fprintf(f, "\n");
 					}
@@ -141,25 +154,27 @@ main(int argc, char **argv)
 					// Ruler Escape
 					// TODO: We should decode more
 					debug_printf("<<<Ruler Escape>>>");
+					unsigned char *escape = (unsigned char *)&payload[j + 1];
+					unsigned char alignment = escape[22] & 3;
+					char *s;
+					switch (alignment) {
+						case 0:
+							s = "left";
+							break;
+						case 1:
+							s = "center";
+							break;
+						case 2:
+							s = "right";
+							break;
+						case 3:
+							s = "justify";
+							break;
+					}
 					if (print_html) {
-						unsigned char *escape = (unsigned char *)&payload[j + 1];
-						unsigned char alignment = escape[22] & 3;
-						char *s;
-						switch (alignment) {
-							case 0:
-								s = "left";
-								break;
-							case 1:
-								s = "center";
-								break;
-							case 2:
-								s = "right";
-								break;
-							case 3:
-								s = "justify";
-								break;
-						}
 						fprintf(f, "<span align=\"%s\">", s);
+					} else if (print_rtf) {
+						fprintf(f, "\\q%c ", s[0]);
 					}
 					j += 26;
 					continue;
@@ -169,12 +184,19 @@ main(int argc, char **argv)
 					unsigned char *escape = (unsigned char *)&payload[j + 1];
 					debug_printf("<<<NewCardSet Escape %02x/%02x/%02x>>>", escape[0], escape[1], escape[2]);
 
-					if (print_html) {
+					if (print_html || print_rtf) {
 						int new_font = escape[0] | escape[1] << 8;
 						int new_font_id = new_font >> 5;
 						int new_font_size = new_font & 0x1F;
 						debug_printf("<<<Font Id=%d Size=%d>>>", new_font_id, new_font_size);
-						fprintf(f, "<span style=\"font-size: %dpt\">", new_font_size);
+						if (new_font_size != font_size) {
+							if (print_html) {
+								fprintf(f, "<span style=\"font-size: %dpt\">", new_font_size);
+							} else {
+								fprintf(f, "\\fs%d ", new_font_size * 2);
+							}
+							font_size = new_font_size;
+						}
 
 						char new_style = escape[2];
 						for (int on = 0; on <= 1; on++) {
@@ -182,35 +204,89 @@ main(int argc, char **argv)
 								char bit = (style >> b) & 1;
 								char new_bit = (new_style >> b) & 1;
 								if (bit != new_bit && new_bit == on) {
-									if (new_bit) {
-										fprintf(f, "<");
+									if (print_html) {
+										if (new_bit) {
+											fprintf(f, "<");
+										} else {
+											fprintf(f, "</");
+										}
+										switch (b) {
+											case 7:
+												fprintf(f, "u");
+												break;
+											case 6:
+												fprintf(f, "b");
+												break;
+											case 5:
+												fprintf(f, "reverse"); // XXX not an actual HTML tag
+												break;
+											case 4:
+												fprintf(f, "i");
+												break;
+											case 3:
+												fprintf(f, "outline"); // XXX not an actual HTML tag
+												break;
+											case 2:
+												fprintf(f, "sup");
+												break;
+											case 1:
+												fprintf(f, "sub");
+												break;
+										}
+										fprintf(f, ">");
 									} else {
-										fprintf(f, "</");
+										switch (b) {
+											case 7:
+												if (new_bit) {
+													fprintf(f, "\\ul ");
+												} else {
+													fprintf(f, "\\ulnone ");
+												}
+												break;
+											case 6:
+												if (new_bit) {
+													fprintf(f, "\\b ");
+												} else {
+													fprintf(f, "\\b0 ");
+												}
+												break;
+											case 5:
+												if (new_bit) {
+													fprintf(f, "{\\colortbl;\\red0\\green0\\blue0;\\red255\\green255\\blue255;}\\cb1\\cf2 ");
+												} else {
+													fprintf(f, "{\\colortbl;\\red0\\green0\\blue0;\\red255\\green255\\blue255;}\\cb2\\cf1 ");
+												}
+												break;
+											case 4:
+												if (new_bit) {
+													fprintf(f, "\\i ");
+												} else {
+													fprintf(f, "\\i0 ");
+												}
+												break;
+											case 3:
+												if (new_bit) {
+													fprintf(f, "\\outl\\strokewidth60 ");
+												} else {
+													fprintf(f, "\\outl0\\strokewidth0 ");
+												}
+												break;
+											case 2:
+												if (new_bit) {
+													fprintf(f, "\\super ");
+												} else {
+													fprintf(f, "\\nosupersub ");
+												}
+												break;
+											case 1:
+												if (new_bit) {
+													fprintf(f, "\\sub ");
+												} else {
+													fprintf(f, "\\nosupersub ");
+												}
+												break;
+										}
 									}
-									switch (b) {
-										case 7:
-											fprintf(f, "u");
-											break;
-										case 6:
-											fprintf(f, "b");
-											break;
-										case 5:
-											fprintf(f, "reverse"); // XXX not an actual HTML tag
-											break;
-										case 4:
-											fprintf(f, "i");
-											break;
-										case 3:
-											fprintf(f, "outline"); // XXX not an actual HTML tag
-											break;
-										case 2:
-											fprintf(f, "sup");
-											break;
-										case 1:
-											fprintf(f, "sub");
-											break;
-									}
-									fprintf(f, ">");
 								}
 							}
 						}
@@ -230,11 +306,24 @@ main(int argc, char **argv)
 					debug_printf("<<<Unknown Escape 0xF5>>>");
 					j += 10;
 					continue;
+				case '{':
+				case '}':
+					if (print_rtf) {
+						fprintf(f, "\\%c", c);
+						continue;
+					} else {
+						break;
+					}
+
 			}
 			fprintf(f, "%c", c);
 		}
 		payload += gross_size;
 		debug_printf("<<<New Page>>>");
+	}
+
+	if (print_rtf) {
+		fprintf(f, "}");
 	}
 
 	fclose(f);
